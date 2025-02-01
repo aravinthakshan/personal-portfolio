ee384c209ff8e68a2dc9946f9e6e82ab979dc8dd
aravinthakshan <aaravinth2005@gmail.com>
2025-02-01 12:17:28 +0530

    route to domain

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,17 @@ import Navigation from "./components/Navigation";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    "https://aravinthakshan-git-main-aravinthakshans-projects.vercel.app/"
+  ),
   title: "A S Aravinthakshan - ML Engineer & Full Stack Developer",
   description:
     "Portfolio of A S Aravinthakshan - Machine Learning Engineer and Full Stack Developer",
-
+  icons: {
+    icon: [{ url: "/favicon.ico" }, { url: "/icon.png", type: "image/png" }],
+    apple: [{ url: "/apple-icon.png" }],
+    shortcut: ["/favicon.ico"],
+  },
   openGraph: {
     type: "website",
     title: "A S Aravinthakshan - Portfolio",
@@ -18,10 +25,10 @@ export const metadata: Metadata = {
     siteName: "A S Aravinthakshan",
     images: [
       {
-        url: "/favicon.png",
+        url: "/preview.png",
         width: 1200,
         height: 630,
-        alt: "A S Aravinthakshan Portfolio Preview",
+        alt: "A S Aravinthakshan Portfolio preview.png",
       },
     ],
     locale: "en_US",
@@ -32,21 +39,21 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "A S Aravinthakshan - Portfolio",
     description: "ML Engineer & Full Stack Developer Portfolio",
-    images: ["/favicon.png"],
+    images: ["/preview.png"],
     creator: "@yourusername",
   },
 
   // Additional metadata for other platforms
   other: {
-    "discord:image": "/favicon.png",
+    "discord:image": "/preview.png",
     "discord:title": "A S Aravinthakshan - Portfolio",
     "discord:description": "ML Engineer & Full Stack Developer Portfolio",
 
-    "linkedin:image": "/favicon.png",
+    "linkedin:image": "/preview.png",
     "linkedin:title": "A S Aravinthakshan - Portfolio",
     "linkedin:description": "ML Engineer & Full Stack Developer Portfolio",
 
-    "whatsapp:image": "/favicon.png",
+    "whatsapp:image": "/preview.png",
     "whatsapp:title": "A S Aravinthakshan - Portfolio",
     "whatsapp:description": "ML Engineer & Full Stack Developer Portfolio",
   },
